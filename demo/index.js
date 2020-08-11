e120fc9499a884e125e0f2d3a04db0906452ca50
angleshe <478647464@qq.com>
2020-08-11 11:32:58 +0800

    ci: 添加demo webpack配置

--- demo/index.js
+++ demo/index.js
@@ -1,1 +1,5 @@
-import '../src/index';
+import React from 'react';
+import { render } from 'react-dom';
+import Editor from '../src/index';
+const App = () => <Editor contentId="gg" />;
+render(<App />, document.getElementById('root'));
