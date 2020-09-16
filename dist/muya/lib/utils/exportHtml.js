5f4851471b73ce94697ebb1312471f3c9306a573
chenyaoming <chenyaoming@wiz.cn>
2020-09-16 10:38:16 +0800

    build

--- dist/muya/lib/utils/exportHtml.js
+++ dist/muya/lib/utils/exportHtml.js
@@ -13,16 +13,6 @@ var _katex = _interopRequireDefault(require('katex'));
 
 var _renderers = _interopRequireDefault(require('../renderers'));
 
-var _githubMarkdown = _interopRequireDefault(require('github-markdown-css/github-markdown.css'));
-
-var _exportStyle = _interopRequireDefault(require('../assets/styles/exportStyle.css'));
-
-var _prism = _interopRequireDefault(require('prismjs/themes/prism.css'));
-
-var _katex2 = _interopRequireDefault(require('katex/dist/katex.css'));
-
-var _headerFooterStyle = _interopRequireDefault(require('../assets/styles/headerFooterStyle.css'));
-
 var _config = require('../config');
 
 var _utils = require('../utils');
@@ -31,6 +21,45 @@ var _emojis = require('../ui/emojis');
 
 function _interopRequireDefault(obj) {
   return obj && obj.__esModule ? obj : { default: obj };
+}
+
+function _getRequireWildcardCache() {
+  if (typeof WeakMap !== 'function') return null;
+  var cache = new WeakMap();
+  _getRequireWildcardCache = function () {
+    return cache;
+  };
+  return cache;
+}
+
+function _interopRequireWildcard(obj) {
+  if (obj && obj.__esModule) {
+    return obj;
+  }
+  if (obj === null || (typeof obj !== 'object' && typeof obj !== 'function')) {
+    return { default: obj };
+  }
+  var cache = _getRequireWildcardCache();
+  if (cache && cache.has(obj)) {
+    return cache.get(obj);
+  }
+  var newObj = {};
+  var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor;
+  for (var key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null;
+      if (desc && (desc.get || desc.set)) {
+        Object.defineProperty(newObj, key, desc);
+      } else {
+        newObj[key] = obj[key];
+      }
+    }
+  }
+  newObj.default = obj;
+  if (cache) {
+    cache.set(obj, newObj);
+  }
+  return newObj;
 }
 
 function _defineProperty(obj, key, value) {
@@ -227,13 +256,28 @@ class ExportHtml {
   async generate(options) {
     const { printOptimization } = options; // WORKAROUND: Hide Prism.js style when exporting or printing. Otherwise the background color is white in the dark theme.
 
+    const githubMarkdownCss = await Promise.resolve().then(() =>
+      _interopRequireWildcard(require('github-markdown-css/github-markdown.css'))
+    );
+    const exportStyle = await Promise.resolve().then(() =>
+      _interopRequireWildcard(require('../assets/styles/exportStyle.css'))
+    );
+    const katexCss = await Promise.resolve().then(() =>
+      _interopRequireWildcard(require('katex/dist/katex.css'))
+    );
+    const footerHeaderCss = await Promise.resolve().then(() =>
+      _interopRequireWildcard(require('../assets/styles/headerFooterStyle.css'))
+    );
+    const highlightCss = await Promise.resolve().then(() =>
+      _interopRequireWildcard(require('prismjs/themes/prism.css'))
+    );
     const highlightCssStyle = printOptimization
-      ? '@media print { '.concat(_prism.default, ' }')
-      : _prism.default;
+      ? '@media print { '.concat(highlightCss, ' }')
+      : highlightCss;
 
     const html = this._prepareHtml(await this.renderHtml(), options);
 
-    const katexCssStyle = this.mathRendererCalled ? _katex2.default : '';
+    const katexCssStyle = this.mathRendererCalled ? katexCss : '';
     this.mathRendererCalled = false; // `extraCss` may changed in the mean time.
 
     const { title, extraCss } = options;
@@ -242,13 +286,13 @@ class ExportHtml {
         (0, _utils.sanitize)(title, _config.EXPORT_DOMPURIFY_CONFIG),
         '</title>\n  <style>\n  '
       )
-      .concat(_githubMarkdown.default, '\n  </style>\n  <style>\n  ')
+      .concat(githubMarkdownCss, '\n  </style>\n  <style>\n  ')
       .concat(highlightCssStyle, '\n  </style>\n  <style>\n  ')
       .concat(
         katexCssStyle,
         '\n  </style>\n  <style>\n    .markdown-body {\n      box-sizing: border-box;\n      min-width: 200px;\n      max-width: 980px;\n      margin: 0 auto;\n      padding: 45px;\n    }\n\n    @media not print {\n      .markdown-body {\n        padding: 45px;\n      }\n\n      @media (max-width: 767px) {\n        .markdown-body {\n          padding: 15px;\n        }\n      }\n    }\n\n    .hf-container {\n      color: #24292e;\n      line-height: 1.3;\n    }\n\n    .markdown-body .highlight pre,\n    .markdown-body pre {\n      white-space: pre-wrap;\n    }\n    .markdown-body table {\n      display: table;\n    }\n    .markdown-body img[data-align="center"] {\n      display: block;\n      margin: 0 auto;\n    }\n    .markdown-body img[data-align="right"] {\n      display: block;\n      margin: 0 0 0 auto;\n    }\n    .markdown-body li.task-list-item {\n      list-style-type: none;\n    }\n    .markdown-body li > [type=checkbox] {\n      margin: 0 0 0 -1.3em;\n    }\n    .markdown-body input[type="checkbox"] ~ p {\n      margin-top: 0;\n      display: inline-block;\n    }\n    .markdown-body ol ol,\n    .markdown-body ul ol {\n      list-style-type: decimal;\n    }\n    .markdown-body ol ol ol,\n    .markdown-body ol ul ol,\n    .markdown-body ul ol ol,\n    .markdown-body ul ul ol {\n      list-style-type: decimal;\n    }\n  </style>\n  <style>'
       )
-      .concat(_exportStyle.default, '</style>\n  <style>')
+      .concat(exportStyle, '</style>\n  <style>')
       .concat(extraCss, '</style>\n</head>\n<body>\n  ')
       .concat(html, '\n</body>\n</html>');
   }
@@ -268,9 +312,9 @@ class ExportHtml {
     }
 
     if (!options.extraCss) {
-      options.extraCss = _headerFooterStyle.default;
+      options.extraCss = footerHeaderCss;
     } else {
-      options.extraCss = _headerFooterStyle.default + options.extraCss;
+      options.extraCss = footerHeaderCss + options.extraCss;
     }
 
     let output = HF_TABLE_START;
