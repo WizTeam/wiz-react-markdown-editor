e5550e1543ef0a51cc708dd6f1eb44550fab1f8f
wiz-sheanqi <sheanqi@wiz.cn>
2020-10-16 15:48:24 +0800

    feat: 添加header功能

--- src/hooks/useImperative.js
+++ src/hooks/useImperative.js
@@ -19,6 +19,36 @@ export default function useImperative(ref, editor) {
   useImperativeHandle(
     ref,
     () => {
+      function insertHeader() {
+        const { start: oldStart } = editor.contentState.cursor;
+        if (oldStart) {
+          const block = editor.contentState.getBlock(oldStart.key);
+          const outBlock = editor.contentState.findOutMostBlock(block);
+          switch (outBlock.type) {
+            case 'h1':
+              editor.contentState.updateParagraph('heading 2', true);
+              break;
+            case 'h2':
+              editor.contentState.updateParagraph('heading 3', true);
+              break;
+            case 'h3':
+              editor.contentState.updateParagraph('heading 4', true);
+              break;
+            case 'h4':
+              editor.contentState.updateParagraph('heading 5', true);
+              break;
+            case 'h5':
+              editor.contentState.updateParagraph('heading 6', true);
+              break;
+            case 'h6':
+            case 'p':
+              editor.contentState.updateParagraph('heading 1', true);
+              break;
+            default:
+              break;
+          }
+        }
+      }
       function insertTag() {
         editor.contentState.formatTag();
       }
@@ -172,6 +202,7 @@ export default function useImperative(ref, editor) {
         editor.contentState.unindent();
       }
       return {
+        insertHeader,
         insertTag,
         insertBold,
         insertItalic,
