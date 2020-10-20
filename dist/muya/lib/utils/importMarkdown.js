68365731684d7e01964a6472edb682077e4e4519
wiz-sheanqi <sheanqi@wiz.cn>
2020-10-20 14:52:35 +0800

    fix: 修改默认光标位置

--- dist/muya/lib/utils/importMarkdown.js
+++ dist/muya/lib/utils/importMarkdown.js
@@ -702,16 +702,21 @@ const importRegister = ContentState => {
     if (hasCursor) {
       travel(this.blocks);
     } else {
-      const lastBlock = this.getLastBlock();
-      const key = lastBlock.key;
-      const offset = lastBlock.text.length;
+      // const lastBlock = this.getLastBlock()
+      // const key = lastBlock.key
+      // const offset = lastBlock.text.length
+      // cursor.anchor = { key, offset }
+      // cursor.focus = { key, offset }
+      // 默认光标在开头
+      const firstBlock = this.getFirstBlock();
+      const key = firstBlock.key;
       cursor.anchor = {
         key,
-        offset
+        offset: 0
       };
       cursor.focus = {
         key,
-        offset
+        offset: 0
       };
     }
 
