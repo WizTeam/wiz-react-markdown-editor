b28959b26d6f200a5002957c86685d587c679040
wiz-sheanqi <sheanqi@wiz.cn>
2020-10-30 17:38:46 +0800

    fix: fix a bug

--- src/muya/lib/contentState/tocCtrl.js
+++ src/muya/lib/contentState/tocCtrl.js
@@ -7,8 +7,10 @@ const tocCtrl = ContentState => {
       if (/^h\d$/.test(block.type)) {
         const { headingStyle, key, type } = block
         const { text } = block.children[0]
+        console.log('text', text)
         const content = headingStyle === 'setext' ? text.trim() : text.replace(/^ *#{1,6} {1,}/, '').trim()
-        const plainContent = text.replace(/^ *#{1,6} {1,}/, '').trim()
+        const plainContent = text.replace(/^ *#{1,6}( | ){1,}/, '').trim()
+        console.log('plainContent', plainContent)
         const lvl = +type.substring(1)
         const slug = key
         toc.push({
