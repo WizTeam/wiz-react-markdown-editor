a6c1e0e2db0233142829131def0301dfebd37dd0
wiz-sheanqi <sheanqi@wiz.cn>
2020-10-30 17:41:06 +0800

    fix: fix a bug

--- dist/muya/lib/contentState/tocCtrl.js
+++ dist/muya/lib/contentState/tocCtrl.js
@@ -22,15 +22,13 @@ const tocCtrl = ContentState => {
         const {
           text
         } = block.children[0];
-        const content = headingStyle === 'setext' ? text.trim() : text.replace(/^ *#{1,6} {1,}/, '').trim();
-        const plainContent = text.replace(/^ *#{1,6}( | ){1,}/, '').trim();
+        const content = headingStyle === 'setext' ? text.trim() : text.replace(/^ *#{1,6}( | ){1,}/, '').trim();
         const lvl = +type.substring(1);
         const slug = key;
         toc.push({
           content,
           lvl,
-          slug,
-          plainContent
+          slug
         });
       }
     }
