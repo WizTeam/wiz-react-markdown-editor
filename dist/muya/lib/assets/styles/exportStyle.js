bcba620240ef1007b7ffa6c57c07e70a8110f91e
zTreeAPI <hunter.z@263.net>
2020-11-04 12:20:00 +0800

    调整 build 脚本，Actions 发布测试

--- dist/muya/lib/assets/styles/exportStyle.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', {
-  value: true
-});
-exports.default = void 0;
-var _default =
-  '\n.markdown-body a.footnote-ref {\n  text-decoration: none;\n}\n\n.footnotes {\n  font-size: .85em;\n  opacity: .8;\n}\n\n.footnotes li[role="doc-endnote"] {\n  position: relative;\n}\n\n.footnotes .footnote-back {\n  position: absolute;\n  font-family: initial;\n  top: .2em;\n  right: 1em;\n  text-decoration: none;\n}\n\n.inline-math.invalid,\n.multiple-math.invalid {\n  color: rgb(255, 105, 105);\n}\n\n';
-exports.default = _default;
